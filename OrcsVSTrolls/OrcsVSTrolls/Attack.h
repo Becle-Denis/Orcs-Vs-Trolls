0652a26ee7779540ec9f2c3a8e534faee311bfe9
(Student) - Denis Bastien Becle <c00254311@ITCARLOW.IE>
2019-10-07 10:53:42 +0100

    integration confict resolved v1

--- OrcsVSTrolls/OrcsVSTrolls/Attack.h
+++ OrcsVSTrolls/OrcsVSTrolls/Attack.h
@@ -8,9 +8,11 @@
 #include "Shield.h"
 //#include "Character.h" integration Conflict 
 
+class Character; //further declaration , avoid conflict 
+
 class Attack : public GameObject {
 public :
-	//virtual void doAttack(Character * attacker, Character * defender, Shield * shield, int meleeBonusMalus = 0, int magicBonusMalus = 0) = 0;
+	virtual void doAttack(Character * attacker, Character * defender, Shield * shield, int meleeBonusMalus = 0, int magicBonusMalus = 0) = 0;
 };
 
 #endif // !ATTACK
