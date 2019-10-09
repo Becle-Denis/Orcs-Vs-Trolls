8dc171ae1759b3c6d1c68baaca67833c626a8327
Denis <c00254311@itcarlow.ie>
2019-10-09 15:59:53 +0100

    Shield implemented

--- OrcsVSTrolls/OrcsVSTrolls/Shield.cpp
+++ OrcsVSTrolls/OrcsVSTrolls/Shield.cpp
@@ -1,1 +1,67 @@
 #include "Shield.h"
+#include "Interface.h"
+#include <string>
+
+
+//--------------------------------COSTRUCTORS / Destructors----------------------------
+
+Shield::Shield(int lifePoint) :
+	m_livePoint(lifePoint),
+	m_destroyed(false)
+{
+
+}
+
+Shield::~Shield() 
+{
+
+}
+
+//---------------------------------Getters-----------------------------------------------
+
+bool Shield::isWorking()
+{
+	return !m_destroyed;
+}
+
+//---------------------------------Modifier----------------------------------------------
+
+void Shield::gainLife(int lifePoint)
+{
+	if (lifePoint > 0)
+	{
+		Interface::display(this->toString() + " + " + std::to_string(lifePoint) + " life points");
+		m_livePoint += lifePoint;
+	}
+}
+
+void Shield::takeDamage(int damagePoint)
+{
+	if (damagePoint > 0)
+	{
+		Interface::display(this->toString() + " - " + std::to_string(damagePoint) + " life points");
+		m_livePoint -= damagePoint;
+	}
+
+	if (m_livePoint <= 0)
+	{
+		Interface::display(this->toString() + " is destroyed");
+		m_destroyed = true;
+	}
+}
+
+//---------------------------------Actions-----------------------------------------------
+
+int Shield::defend(int& meleeAttackPoint, int& magicAttackPoint, int directShieldDamage)
+{
+	if (this->isWorking())
+	{
+		Interface::display(this->toString() + " protect");
+		return this->protect(meleeAttackPoint, magicAttackPoint, directShieldDamage);
+	}
+	else 
+	{
+		Interface::display(this->toString() + " is destroyed or cancelled");
+		return 0;
+	}
+}
--- OrcsVSTrolls/OrcsVSTrolls/Shield.h
+++ OrcsVSTrolls/OrcsVSTrolls/Shield.h
@@ -11,6 +11,9 @@
 class Shield : public GameObject {
 
 public :
+
+	Shield(int lifePoint);
+	~Shield();
 	/// <summary>
 	/// protect the attack 
 	/// return by reference
@@ -19,14 +22,22 @@ public :
 	/// <param name="magicAttackPoint">initial MagicAttackPoint, return final</param>
 	/// <param name="directShieldDamage">damage to the shield</param>
 	/// <returns>DamageToTheDefenders</returns>
-	virtual int protect(int & meleeAttackPoint,int & magicAttackPoint, int directShieldDamage = 0) = 0;
+	virtual int defend(int& meleeAttackPoint, int& magicAttackPoint, int directShieldDamage = 0);
+	
 
 	bool isWorking();
-private : 
-	bool destroyed;
-	int livePoint;
-	int meleeProtection;
-	int magicProtection;
+	void takeDamage(int damagePoint);
+	virtual void gainLife(int lifePoint);
+
+	//Overrided GameObject functions
+	std::string toString() = 0;
+	std::string toStringDescription() = 0 ;
+
+
+protected : 
+	bool m_destroyed;
+	int m_livePoint;
+	virtual int protect(int& meleeAttackPoint, int& magicAttackPoint, int directShieldDamage = 0) = 0;
 };
 
 #endif // !SHIELD
